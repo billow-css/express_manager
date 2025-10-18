63ab14b96935f65ce592d5fe47dafcf4c467a7a2
billow-css <1564791960@qq.com>
2025-10-18 17:07:58 +0800

    commit 0001-1 : member class

---
+++ member.h
@@ -0,0 +1,37 @@
+# pragma once
+# include <string>
+
+class Member{
+    public:
+        Member() = delete;
+        Member(const std::string& name, const std::string& phone, const std::string& address)
+            : name(name), phone(phone), address(address) {}
+
+        std::string getName() const {
+            return name;
+        }
+
+        std::string getPhone() const {
+            return phone;
+        }
+
+        std::string getAddress() const {
+            return address;
+        }
+
+        void editName(const std::string& newName) {
+            name = newName;
+        }
+
+        void editPhone(const std::string& newPhone) {
+            phone = newPhone;
+        }
+
+        void editAddress(const std::string& newAddress) {
+            address = newAddress;
+        }
+    private:
+        std::string name;
+        std::string phone;
+        std::string address;
+};
